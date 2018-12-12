3d360becbf23a99b55dd756f1e4cddbd0ad739ae
icesygarE <34585040322qq.comm>
2018-12-12 10:00:25 +0800

    12.12

--- School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/MineFragment.java
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/MineFragment.java
@@ -3,15 +3,18 @@ package com.w.school_herper_front.HomePage.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Message;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.w.school_herper_front.Authen.AuthenNoActivity;
 import com.w.school_herper_front.Message.MessageSetActivity;
 import com.w.school_herper_front.R;
+import com.w.school_herper_front.wallet.WalletActivity;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,7 +22,7 @@ import com.w.school_herper_front.R;
 public class MineFragment extends Fragment {
     
     private TextView setMessage;
-
+    private ImageView moneybag;
 
     public MineFragment() {
         // Required empty public constructor
@@ -33,13 +36,28 @@ public class MineFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_mine, container, false);
         setMessage = view.findViewById(R.id.tv_set);
+        moneybag = view.findViewById(R.id.img_money);
         // Inflate the layout for this fragment
 
+
+        /*
+         * 姓名：赵璐
+         * 日期：2018.12.12
+         * 说明：基本的页面跳转
+         * */
         //跳转到设置页面
         setMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getActivity(),MessageSetActivity.class);
+                Intent intent = new Intent(getActivity(), MessageSetActivity.class);
+                startActivity(intent);
+            }
+        });
+        //跳转到钱包
+        moneybag.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), WalletActivity.class);
                 startActivity(intent);
             }
         });
--- School_Herper_front/app/src/main/java/com/w/school_herper_front/Message/MessageSetActivity.java
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/Message/MessageSetActivity.java
@@ -2,27 +2,47 @@ package com.w.school_herper_front.Message;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import com.w.school_herper_front.Authen.AuthenNoActivity;
 import com.w.school_herper_front.ChangeKeyActivity;
 import com.w.school_herper_front.ChangePassword.ChangePassword;
 import com.w.school_herper_front.ForgotKeyActivity;
+import com.w.school_herper_front.HomePage.HomeActivity;
 import com.w.school_herper_front.MainActivity;
 import com.w.school_herper_front.R;
 
 public class MessageSetActivity extends Activity {
     private LinearLayout authen;
     private LinearLayout changepsd;
+    private ImageView back1;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.message_set);
         authen = findViewById(R.id.ln_authens);
         changepsd = findViewById(R.id.ln_changepsd);
+        back1 = findViewById(R.id.back_1);
+
+        /*
+         * 姓名：赵璐
+         * 日期：2018.12.12
+         * 说明：基本的页面跳转
+         * */
+        //返回个人主页
+        authen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MessageSetActivity.this,HomeActivity.class);
+                intent.putExtra("id",1);
+                startActivity(intent);
+            }
+        });
 
         //跳转到认证页面
         authen.setOnClickListener(new View.OnClickListener() {
