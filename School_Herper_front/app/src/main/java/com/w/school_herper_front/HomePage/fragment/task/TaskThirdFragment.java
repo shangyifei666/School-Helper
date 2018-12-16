17c0770658883de6f30a1b103b04333c9a602ac1
shangyifei666 <1004978291@qq.com>
2018-12-16 16:04:11 +0800

    任务页子界面修改

---
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/task/TaskThirdFragment.java
@@ -0,0 +1,30 @@
+package com.w.school_herper_front.HomePage.fragment.task;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.w.school_herper_front.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TaskThirdFragment extends Fragment {
+
+
+    public TaskThirdFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_task_third, container, false);
+    }
+
+}
