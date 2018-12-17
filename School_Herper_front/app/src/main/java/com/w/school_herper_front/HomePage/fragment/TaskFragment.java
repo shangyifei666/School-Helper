078dcb3720dcedcae324f2a08452f3907bd03af3
shangyifei666 <1004978291@qq.com>
2018-12-17 16:06:33 +0800

    新建3个activity删除fragment

--- School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/TaskFragment.java
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/TaskFragment.java
@@ -1,11 +1,8 @@
 package com.w.school_herper_front.HomePage.fragment;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,7 +10,6 @@ import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.w.school_herper_front.HomePage.fragment.task.TaskAdapter;
-import com.w.school_herper_front.HomePage.fragment.task.TaskFirstFragment;
 import com.w.school_herper_front.HomePage.fragment.task.task;
 import com.w.school_herper_front.R;
 
---
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/HomePage/fragment/task/TaskFirstActivity.java
@@ -0,0 +1,15 @@
+package com.w.school_herper_front.HomePage.fragment.task;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.w.school_herper_front.R;
+
+public class TaskFirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_first);
+    }
+}
