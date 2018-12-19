0cb5f06cbad08eb77f719a7fc84a187e3229906c
yangxuhui111 <790443530@qq.com>
2018-12-19 11:13:33 +0800

    数据库更新

--- Mysql/school_helper.sql
+++ Mysql/school_helper.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : zhang
-Source Server Version : 80013
+Source Server         : work
+Source Server Version : 80012
 Source Host           : localhost:3306
 Source Database       : school_helper
 
 Target Server Type    : MYSQL
-Target Server Version : 80013
+Target Server Version : 80012
 File Encoding         : 65001
 
-Date: 2018-12-12 15:26:14
+Date: 2018-12-19 11:12:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -87,16 +87,21 @@ CREATE TABLE `user` (
   `user_reputation_value` int(10) NOT NULL,
   `user_took_count` int(10) NOT NULL,
   `user_publish_count` int(10) NOT NULL,
-  `user_identification` int(10) NOT NULL,
-  `user_signature` varchar(50) NOT NULL DEFAULT '',
-  `user_realname` varchar(25) NOT NULL,
-  `user_sex` varchar(25) NOT NULL,
+  `user_identification` varchar(10) NOT NULL,
+  `user_signature` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT '',
+  `user_realname` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `user_sex` varchar(25) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`user_id`),
   UNIQUE KEY `user_id` (`user_id`),
   KEY `school_id` (`school_id`),
   CONSTRAINT `user_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `school` (`school_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
+INSERT INTO `user` VALUES ('1', '111', '111', '1', '111', '111', 'images/geren.png', '0.00', '60', '0', '0', '0', '个性标签', '真实姓名', '男');
+INSERT INTO `user` VALUES ('2', 'xioyoutiao', '272884559', '1', '201699999', '199999999', 'images/geren.png', '0.00', '60', '0', '0', '0', '个性标签', '真实姓名', '男');
+INSERT INTO `user` VALUES ('3', '222', '222', '1', '222', '222', 'images/geren.png', '0.00', '60', '0', '0', '0', null, null, null);
+INSERT INTO `user` VALUES ('4', 'xiaoxigua', '999', '1', '999', '999', 'images/geren.png', '0.00', '60', '0', '0', '', 'wohenfanfanfnafnanf', 'liudahau', 'nv');
+INSERT INTO `user` VALUES ('5', '88', '888', '1', '88', '888', 'images/geren.png', '0.00', '60', '0', '0', '未认证', '99', '99', '99');
