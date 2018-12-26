218e63c288b3fb35f8d9296e6a8b456348e665a4
shangyifei666 <1004978291@qq.com>
2018-12-26 16:26:41 +0800

    服务器的地址更新

--- School_Herper_front/app/src/main/java/com/w/school_herper_front/ServerUrl.java
+++ School_Herper_front/app/src/main/java/com/w/school_herper_front/ServerUrl.java
@@ -9,7 +9,7 @@ package com.w.school_herper_front;
 * 描述：方便修改服务器url
 * */
 public class ServerUrl {
-    private String url = "http://10.7.89.234:8080";
+    private String url = "http://47.94.19.82";
 
     public String getUrl(){return url;}
 }
